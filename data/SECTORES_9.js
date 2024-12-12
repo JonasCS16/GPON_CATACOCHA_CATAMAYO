6a623c6307521de64f876a2a8cf2525f207908e8
JonasCS16 <jonascs1692@gmail.com>
2024-12-11 21:34:12 -0500

    Add files via upload

---
+++ data/SECTORES_9.js
@@ -0,0 +1,12 @@
+var json_SECTORES_9 = {
+"type": "FeatureCollection",
+"name": "SECTORES_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "San Pedro Martir" }, "geometry": { "type": "Point", "coordinates": [ -79.627280970542643, -4.034621869743799 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Puente de San Pedro Martir" }, "geometry": { "type": "Point", "coordinates": [ -79.621863444870172, -4.035221006790037 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "El Placer" }, "geometry": { "type": "Point", "coordinates": [ -79.615946388839276, -4.033239700416981 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Naranjo Palto" }, "geometry": { "type": "Point", "coordinates": [ -79.608304763714031, -4.023161064216914 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Barrio Chapango" }, "geometry": { "type": "Point", "coordinates": [ -79.635775588355997, -4.039508939125804 ] } }
+]
+}
